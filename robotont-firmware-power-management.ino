e0baadb973010d613f79b069f582a4e91f9c0bbc
Norpz <112713620+Norpz@users.noreply.github.com>
2024-02-28 18:07:30 +0200

    Initial power management firmware, ESTOP MISSING

--- robotont-firmware-power-management.ino
+++ robotont-firmware-power-management.ino
@@ -1,58 +1,167 @@
-#define PIN_DBG_LED_G PD6
-#define PIN_DBG_LED_R PD5
-#define PIN_POWER_SW PD2
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdio.h>
+
+//GATES
 #define PIN_BAT_PWR_CTRL 7 // PortB
-#define PIN_WALL_PWR_CTRL PD4
 #define PIN_SYS_PWR_CTRL 3 //PortA
 #define PIN_MOTOR_PWR_CTRL 7 //PortC
+#define PIN_WALL_PWR_CTRL 4 //PortD
+//Current sensing pin numbers
+#define PIN_I_MOTORS_SENSE 1 //PORTA
+#define PIN_I_NUC_SENSE 3 //PORTC
+//Voltage sensing pin numbers
+#define PIN_V_SENSE 1 //PORTC
+#define PIN_VBAT_SENSE 2 //PORTC
+#define PIN_POWER_SW 2 //PORTD
+#define PIN_DBG_LED_G 6 //PORTD
+#define PIN_DBG_LED_R 5 //PORTD
+
+//Defines for analogRead function
+#define V_SENSE PC1
+#define VBAT_SENSE PC2
+#define I_MOTORS_SENSE PA1
+#define I_NUC_SENSE PC3
+
+enum PowerState{
+  POWER_OFF,
+  POWER_ON
+};
+
+enum SwitchingState{
+  INIT,
+  CONNECTED_TO_WALL,
+  CONNECTED_TO_BAT
+};
+
 
 
 void setup() {
-  pinMode(PIN_DBG_LED_G, OUTPUT);
-  pinMode(PIN_DBG_LED_R, OUTPUT);
-  //pinMode(PIN_BAT_PWR_CTRL, OUTPUT);
-  bitSet(DDRB, PIN_BAT_PWR_CTRL); //Set as output
-  pinMode(PIN_WALL_PWR_CTRL, OUTPUT);
-  bitSet(DDRA, PIN_SYS_PWR_CTRL);
-  bitSet(DDRA, PIN_MOTOR_PWR_CTRL);
-  pinMode(PIN_POWER_SW, INPUT);
-  digitalWrite(PIN_DBG_LED_R, LOW);
-  digitalWrite(PIN_DBG_LED_G, LOW);
-  //digitalWrite(PIN_BAT_PWR_CTRL, LOW);
-  bitClear(PORTB, PIN_BAT_PWR_CTRL);
-  digitalWrite(PIN_WALL_PWR_CTRL, LOW);
-  bitClear(PORTA, PIN_SYS_PWR_CTRL);
-  bitClear(PORTC, PIN_MOTOR_PWR_CTRL);
+  
+  //Current Sensing pins as INPUT
+  DDRC &= ~(1 << PIN_I_NUC_SENSE);
+  DDRA &= ~(1 << PIN_I_MOTORS_SENSE);
+
+  //Voltage Sensing pins as INPUT
+  DDRC &= ~(1 << PIN_V_SENSE);
+  DDRC &= ~(1 << PIN_VBAT_SENSE);
+
+  //POWER BUTTON as INPUT
+  DDRD &= ~(1 << PIN_POWER_SW);
+
+  //LEDS as OUTPUT
+  DDRC |= (1 << PIN_DBG_LED_R);
+  DDRC |= (1 << PIN_DBG_LED_G);
+  bitWrite(PORTC, PIN_DBG_LED_R, 0);
+  bitWrite(PORTC, PIN_DBG_LED_G, 0);
+
+  //GATES as OUTPUT
+  DDRB |= (1 << PIN_BAT_PWR_CTRL);
+  DDRD |= (1 << PIN_WALL_PWR_CTRL);
+  DDRC |= (1 << PIN_MOTOR_PWR_CTRL);
+  DDRA |= (1 << PIN_SYS_PWR_CTRL);
+
+  //ALL GATES CLOSED
+  bitWrite(PORTB, PIN_BAT_PWR_CTRL, 0);
+  bitWrite(PORTD, PIN_WALL_PWR_CTRL, 0);
+  bitWrite(PORTC, PIN_MOTOR_PWR_CTRL, 0);
+  bitWrite(PORTA, PIN_SYS_PWR_CTRL, 0);
+  
+  const int interruptFrequency = 1500;
+  TCCR0A = (1<<CTC0) | (1<<CS01); //CTC ja kaheksane pre
+  TCNT0  = 0; //Counter nulli
+  OCR0A = 4000000 / (2 *8 *interruptFrequency) - 1; 
+  // Enable Timer/Counter0 Output Compare Match A Interrupt Enable
+  TIMSK0 |= (1 << OCIE0A);
+
+  ADCSRA |= (0 << ADPS2) | (0 << ADPS1) | (0 << ADPS0); //ADC pre minimaalseks
+
+  sei();
+
 }
 
+enum PowerState powerState = POWER_OFF;
+enum SwitchingState switchingState = INIT;
+
 void loop() {
-  //digitalWrite(PIN_BAT_PWR_CTRL, HIGH);
-  digitalWrite(PIN_DBG_LED_G, HIGH); 
-  delay(500);
-  //digitalWrite(PIN_BAT_PWR_CTRL, LOW);                       
-  digitalWrite(PIN_DBG_LED_G, LOW);
-  delay(500);
-  if (!digitalRead(PIN_POWER_SW))
+
+  bitWrite(PORTD, PIN_DBG_LED_G, 1);
+  delay(10000);
+  bitWrite(PORTD, PIN_DBG_LED_G, 0);
+  delay(10000);
+
+  if (!bitRead(PORTD, PIN_POWER_SW))
   {
-    if (!digitalRead(PIN_DBG_LED_R))
+    if (powerState == POWER_OFF)
     {
-      // Turn power from bat to sys and motors on
-      digitalWrite(PIN_DBG_LED_R, HIGH);
-      bitSet(PORTB, PIN_BAT_PWR_CTRL);
-      bitSet(PORTA, PIN_SYS_PWR_CTRL);
-      bitSet(PORTC, PIN_MOTOR_PWR_CTRL);
-      //digitalWrite(PIN_BAT_PWR_CTRL, HIGH);
-      //digitalWrite(PIN_SYS_PWR_CTRL, HIGH);
+      bitWrite(PORTD, PIN_DBG_LED_R, 1);
+      powerState = POWER_ON;
     }
     else
     {
       // Turn power off
-      digitalWrite(PIN_DBG_LED_R, LOW);
-      bitClear(PORTB, PIN_BAT_PWR_CTRL);
-      bitClear(PORTA, PIN_SYS_PWR_CTRL);
-      bitClear(PORTC, PIN_MOTOR_PWR_CTRL);
-      //digitalWrite(PIN_BAT_PWR_CTRL, LOW);
-      //digitalWrite(PIN_SYS_PWR_CTRL, LOW);
+      powerState = POWER_OFF;
+      switchingState = INIT;
+      bitWrite(PORTD, PIN_DBG_LED_G, 0);
+      bitWrite(PORTB, PIN_BAT_PWR_CTRL, 0);
+      bitWrite(PORTD, PIN_WALL_PWR_CTRL, 0);
+      bitWrite(PORTC, PIN_MOTOR_PWR_CTRL, 0);
+      bitWrite(PORTA, PIN_SYS_PWR_CTRL, 0);
     }
   }
+}
+
+//Bat off, wall on
+void switchBatToWall(){
+  bitWrite(PORTA, PIN_SYS_PWR_CTRL, 1); //SYS ON
+  bitWrite(PORTC, PIN_MOTOR_PWR_CTRL, 0); //MOTORS OFF
+  bitWrite(PORTB, PIN_BAT_PWR_CTRL, 0);
+  bitWrite(PORTD, PIN_WALL_PWR_CTRL, 1);
+ 
+  switchingState = CONNECTED_TO_WALL;
+}
+
+//Wall off, bat on
+void switchWallToBat(){
+  bitWrite(PORTA, PIN_SYS_PWR_CTRL, 1); //SYS ON
+  bitWrite(PORTC, PIN_MOTOR_PWR_CTRL, 1); //MOTORS ON
+  bitWrite(PORTD, PIN_WALL_PWR_CTRL, 0);
+  bitWrite(PORTB, PIN_BAT_PWR_CTRL, 1);
+  
+  switchingState = CONNECTED_TO_BAT;
+}
+
+ISR(TIMER0_COMPA_vect) {
+  if(powerState == POWER_ON){
+    uint16_t V = analogRead(V_SENSE);
+    uint16_t VBAT = analogRead(VBAT_SENSE);
+    if(V >= VBAT && (switchingState !=  CONNECTED_TO_BAT)){
+      switchBatToWall();
+    }
+    else if(V < VBAT && (switchingState != CONNECTED_TO_WALL)){
+      switchWallToBat();
+    }
+  }
+}
+
+//Current calculations:
+uint16_t measureMotorCurrent(){
+  //Shunt 5m, Gain 40x
+  // V = (analogRead(I_MOTORS_SENSE) * 3.3)/(1024*40)
+  // I = V / Shunt  ---  I = V * (1/Shunt)
+  //((analogRead(I_MOTORS_SENSE) * 3.3)/1024*40)*200
+  uint16_t MtrCurrent=analogRead(I_MOTORS_SENSE)/62.061; //Amp
+  
+  return MtrCurrent; 
+}
+//0.673 - 1.13a
+uint16_t measureNUCCurrent(){
+  //Shunt 10m, Gain 60x
+  //V = (analogRead(I_MOTORS_SENSE) * 3.3)/(1024*60) //60x gain
+  // I = V / Shunt  ---  I = V * (1/Shunt)
+  //((analogRead(I_NUC_SENSE) * 3.3)/1024*60)*100
+  uint16_t NucCurrent=analogRead(I_NUC_SENSE)/186.182; //Amp
+  
+  return NucCurrent; 
 }
