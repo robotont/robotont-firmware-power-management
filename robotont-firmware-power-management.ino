4cf8790486be10e30bd287b57e7a427efc1adfd2
Norpz <112713620+Norpz@users.noreply.github.com>
2024-03-13 17:31:57 +0200

    Added I2C, custom ADCRead, ESTOP leds(not tested)

--- robotont-firmware-power-management.ino
+++ robotont-firmware-power-management.ino
@@ -2,6 +2,7 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <stdio.h>
+#include <Wire.h>
 
 //IF POSSIBLE DONT USE pinMode() function, some pin mappings are not valid
 
@@ -12,22 +13,24 @@
 #define PIN_MOTOR_PWR_CTRL 7 //PortC
 #define PIN_WALL_PWR_CTRL 4 //PortD
 //Current sensing pin numbers
-#define PIN_I_MOTORS_SENSE 1 //PORTA
-#define PIN_I_NUC_SENSE 3 //PORTC
+#define PIN_I_MOTORS_SENSE 1 //PORTA //ADC channel channel7
+#define PIN_I_NUC_SENSE 3 //PORTC //ADC channel channel 3
 //Voltage sensing pin numbers
-#define PIN_V_SENSE 1 //PORTC
-#define PIN_VBAT_SENSE 2 //PORTC
+#define PIN_V_SENSE 1 //PORTC //ADC channel 1
+#define PIN_VBAT_SENSE 2 //PORTC //ADC channel 2
 #define PIN_POWER_SW 2 //PORTD
 #define PIN_ESTOP_SW 3 //PORTD
 #define PIN_DBG_LED_G 6 //PORTD
 #define PIN_DBG_LED_R 5 //PORTD
+#define ESTOP_LED_R 1 //PORTB
+#define ESTOP_LED_G 2 //PORTG
 #define PIN_BUZZER 0 //PD0
 
 //Defines for analogRead function
-#define V_SENSE PC1
-#define VBAT_SENSE PC2
-#define I_MOTORS_SENSE PA1
-#define I_NUC_SENSE PC3
+#define V_SENSE_ADC 1
+#define VBAT_SENSE_ADC 2
+#define I_MOTORS_SENSE_ADC 7
+#define I_NUC_SENSE_ADC 3
 
 #define POWER_SW PD2
 #define ESTOP_SW PD3
@@ -47,6 +50,9 @@ enum PowerState powerState = POWER_OFF;
 enum SwitchingState switchingState = INIT;
 uint8_t EStopPressed = 0;
 
+uint16_t V;
+uint16_t VBAT;
+
 void setup() {
   
   //Current Sensing pins as INPUT
@@ -59,7 +65,7 @@ void setup() {
 
   //POWER BUTTON as INPUT
   DDRD &= ~(1 << PIN_POWER_SW);
-  //ESTOP as INPUT
+  //ESTOP SWITCH as INPUT
   DDRD &= ~(1 << PIN_ESTOP_SW);
   PCMSK2 |= (1 << PCINT19);   //Enable Pin Change Interrupt
   PCICR |= (1 << PCIE2); // Enable Pin Change Interrupt for Port D
@@ -69,6 +75,11 @@ void setup() {
   DDRD |= (1 << PIN_DBG_LED_G);
   bitWrite(PORTD, PIN_DBG_LED_R, 0);
   bitWrite(PORTD, PIN_DBG_LED_G, 0);
+  
+  DDRB |= (1 << ESTOP_LED_R);
+  DDRB |= (1 << ESTOP_LED_G);
+  bitWrite(PORTB, ESTOP_LED_R, 1);
+  bitWrite(PORTB, ESTOP_LED_G, 1);
 
   //GATES as OUTPUT
   DDRB |= (1 << PIN_BAT_PWR_CTRL);
@@ -90,12 +101,16 @@ void setup() {
   const int interruptFrequency = 1500;
   TCCR0A = (1<<CTC0) | (1<<CS01); //CTC and 8 prescaler
   TCNT0  = 0; //Counter to zero
-  OCR0A = 4000000 / (2 *8 *interruptFrequency) - 1; 
+  OCR0A = 1000000 / (2 *2 *interruptFrequency) - 1; 
   // Enable Timer/Counter0 Output Compare Match A Interrupt Enable
   TIMSK0 |= (1 << OCIE0A);
 
-  ADCSRA |= (0 << ADPS2) | (0 << ADPS1) | (0 << ADPS0); //ADC prescaler to minimal //Not tested
+  
+  ADCSRA |= (1 << ADEN) | (0 << ADPS2) | (0 << ADPS1) | (1 << ADPS0);
+  ADMUX |= (1 << REFS0);
 
+  Wire.begin(); // join i2c bus as master
+  
   sei();
 
 }
@@ -107,6 +122,7 @@ void loop() {
   delay(10000);
   bitWrite(PORTD, PIN_DBG_LED_G, 0);
   delay(10000);
+  sendDataOverI2C();
 
   if (!digitalRead(POWER_SW))
   {
@@ -127,6 +143,24 @@ void loop() {
       bitWrite(PORTA, PIN_SYS_PWR_CTRL, 0);
     }
   }
+}
+
+
+uint16_t ADCRead(uint8_t channel){
+  ADMUX = (ADMUX & 0xF0) | (channel & 0x0F); //Select adc channel
+  // Start conversion
+  ADCSRA |= (1 << ADSC);
+  
+  // Wait for conversion to complete
+  while (ADCSRA & (1 << ADSC));
+  
+  // Read ADCL first
+  uint8_t low = ADCL;
+  // Then read ADCH
+  uint8_t high = ADCH;
+  // Combine low and high
+  return (high << 8) | low;
+  
 }
 
 //Bat off, wall on
@@ -152,8 +186,8 @@ void switchWallToBat(){
 ISR(TIMER0_COMPA_vect) {
   
   if(powerState == POWER_ON){
-    uint16_t V = analogRead(V_SENSE);
-    uint16_t VBAT = analogRead(VBAT_SENSE);
+    V = ADCRead(V_SENSE_ADC); //ADC1
+    VBAT = ADCRead(VBAT_SENSE_ADC); //ADC2
     if(V >= VBAT && (switchingState !=  CONNECTED_TO_BAT)){
       switchBatToWall();
     }
@@ -165,35 +199,45 @@ ISR(TIMER0_COMPA_vect) {
 
 
 ISR(PCINT2_vect) {
-    if(!digitalRead(ESTOP_SW)){ //Button 0 when pressed
+    if(!digitalRead(ESTOP_SW)){ //Button 0 when pressed, RED LED ON
       EStopPressed=1;
       bitWrite(PORTC, PIN_MOTOR_PWR_CTRL, 0);
+      bitWrite(PORTB, ESTOP_LED_R, 0);
+      bitWrite(PORTB, ESTOP_LED_G, 1);
     }
     else{
       EStopPressed=0;
-      if(switchingState == CONNECTED_TO_BAT){
+      if(switchingState == CONNECTED_TO_BAT){ //GREEN LED ON
         bitWrite(PORTC, PIN_MOTOR_PWR_CTRL, 1);
+        bitWrite(PORTB, ESTOP_LED_R, 1);
+        bitWrite(PORTB, ESTOP_LED_G, 0);
       }
     }
 }
 
-//Current calculations:
-uint16_t measureMotorCurrent(){
-  //Shunt 5m, Gain 40x
-  // V = (analogRead(I_MOTORS_SENSE) * 3.3)/(1024*40)
-  // I = V / Shunt  ---  I = V * (1/Shunt)
-  //((analogRead(I_MOTORS_SENSE) * 3.3)/1024*40)*200
-  uint16_t MtrCurrent=analogRead(I_MOTORS_SENSE)/62.061; //Amp
+
+void sendDataOverI2C(){
+
+  //Read currents and send them to STM on request
+  uint16_t MtrCurrent = ADCRead(I_MOTORS_SENSE_ADC);
+  uint16_t NucCurrent = ADCRead(I_NUC_SENSE_ADC);
   
-  return MtrCurrent; 
-}
-//0.673 - 1.13a
-uint16_t measureNUCCurrent(){
-  //Shunt 10m, Gain 60x
-  //V = (analogRead(I_MOTORS_SENSE) * 3.3)/(1024*60) //60x gain
-  // I = V / Shunt  ---  I = V * (1/Shunt)
-  //((analogRead(I_NUC_SENSE) * 3.3)/1024*60)*100
-  uint16_t NucCurrent=analogRead(I_NUC_SENSE)/186.182; //Amp
+  // Convert the numbers to bytes
+  byte byteArr[8];
+  byteArr[0] = highByte(MtrCurrent);
+  byteArr[1] = lowByte(MtrCurrent);
   
-  return NucCurrent; 
+  byteArr[2] = highByte(NucCurrent);
+  byteArr[3] = lowByte(NucCurrent);
+
+  byteArr[4] = highByte(V);
+  byteArr[5] = lowByte(V);
+
+  byteArr[6] = highByte(VBAT);
+  byteArr[7] = lowByte(VBAT);
+
+  Wire.beginTransmission(0x12); //STM'S address
+  Wire.write(byteArr, sizeof(byteArr));
+  Wire.endTransmission();
+  
 }
